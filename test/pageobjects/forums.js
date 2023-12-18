17401587bc7ae8ac3b74ec1d168718d446de3acf
lauren-burton <lauren.burton5390@stu.mtec.edu>
2023-12-18 13:24:17 -0700

    more to the forums(voting)

--- test/pageobjects/forums.js
+++ test/pageobjects/forums.js
@@ -18,12 +18,36 @@ class forums extends Base {
         return $('.tw-break-words > [href="/boardgame/162886/spirit-island"]');
     }
 
+    get complexityRating() {
+        return $('[uib-tooltip="View results"]');
+    }
+
+    get goToPoll() {
+        return $('[ng-click="questions();"]');
+    }
+
+    get mediumHeavy() {
+        return $('//*[contains(text(),"Medium Heavy")]');
+    }
+
+    get vote() {
+        return $('[ng-click="submit();"]');
+    }
+
+    get close() {
+        return $('//button[contains(text(),"Close")]');
+    }
     //opening the forums and clicking hot/spirit island
 
     async forum () {
         await this.forumsTab.click();
         await this.hotLink.click();
         await this.spiritIsland.click();
+        await this.complexityRating.click();
+        await this.goToPoll.click();
+        await this.mediumHeavy.click();
+        await this.vote.click();
+        await this.close.click();
     }
 
 }
